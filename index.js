e663097cd0ee5f2d50a04868071e350ae80dc09a
banghawe <maxnara19@gmail.com>
2023-06-14 15:29:40 +0700

    bump 1.1.2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.info("Loading Wortal.js 1.1.1");
+console.info("Loading Wortal.js 1.1.2");
 
 window.adsbygoogle = window.adsbygoogle || [];
 var PLACEMENTS = ["start", "pause", "next", "browse", "reward", "preroll"]
@@ -39,68 +39,119 @@ function event(name, features) {
 
 window.triggerWortalAd = function (placement, description, callbacks) {
     /**
+     * Doc: https://developers.google.com/ad-placement/apis/adbreak
      * Callbacks is object:
      * {
      *     beforeAd: function () {},
      *     afterAd: function () {},
      *     adBreakDone: function () {},
+     *     beforeReward: function (showAdFn) {},     // Show reward prompt (call showAdFn() if clicked)
+     *     adDismissed: function () {},              // Player dismissed the ad before completion
+     *     adViewed: function () {},                 // Ad was viewed and closed
      *     noShow: function () {}
      * }
      */
-    var adShown = false;
-    let clientId = getParameterByName("clientid");
-    let hostChannelId = getParameterByName("channelid")
-    let hostId = getParameterByName("hostid")
-    let sessionId = getParameterByName('sessid')
 
     if (PLACEMENTS.indexOf(placement) < 0) {
         console.warn("Invalid placement selected:", placement, "Should be one of", PLACEMENTS);
         return;
     }
 
-    setTimeout(function () {
+    var adShown = false;
+    let clientId = getParameterByName("clientid");
+    let hostChannelId = getParameterByName("channelid")
+    let hostId = getParameterByName("hostid")
+    let sessionId = getParameterByName('sessid')
 
-        if (adShown) {
-            event('AdShown', {
-                client_id: clientId,
-                host_channel_id: hostChannelId,
-                host_id: hostId,
-                session_id: sessionId,
-                placement
-            })
-            return;
-        }
-
-        console.warn("Ad did not show. Skipping.");
-        if (typeof callbacks.noShow !== "function") {
-
-            return;
-        }
-
-        callbacks.noShow();
-
-    }, 500);
-
-    console.log("Wortal Ad Break called placement=", placement, "description=", description);
-
-    //{beforeAd, afterAd, adBreakDone, onNoShow}
+    console.info("Wortal Ad Break called placement=", placement, "description=", description);
 
     var params = {
-        type: placement,
-        name: description,
-        beforeAd: function () {
+        type: placement, name: description
+    }
+
+    params.adBreakDone = function (placementInfo) {
+        console.log(placementInfo)
+        event('AdBreakDone', {
+            client_id: clientId,
+            host_channel_id: hostChannelId,
+            host_id: hostId,
+            session_id: sessionId,
+            placement,
+            breakFormat: placementInfo.breakFormat,
+            breakStatus: placementInfo.breakStatus,
+        })
+
+        callbacks.adBreakDone && callbacks.adBreakDone(placementInfo)
+    }
+
+    if (callbacks.beforeReward) {
+        params.beforeReward = callbacks.beforeReward
+    }
+
+    if (callbacks.adDismissed) {
+        params.adDismissed = callbacks.adDismissed
+    }
+
+    if (callbacks.adViewed) {
+        params.adViewed = callbacks.adViewed
+    }
+
+    if (placement === "preroll") {
+
+        console.info("Attempting to show preroll.");
+
+        // Set a timeout to control ads not showing.
+        setTimeout(function () {
+            console.warn("Ad did not show. Skipping.");
+            if (typeof callbacks.noShow !== "function") {
+
+                return;
+            }
+
+            callbacks.noShow();
+
+        }, 500);
+
+        adBreak(params);
+
+    } else {
+
+        console.log("Attempting to show", placement);
+        //{beforeAd, afterAd, adBreakDone, onNoShow}
+
+        // Set a timeout to control ads not showing.
+        setTimeout(function () {
+
+            if (adShown) {
+                event('AdShown', {
+                    client_id: clientId,
+                    host_channel_id: hostChannelId,
+                    host_id: hostId,
+                    session_id: sessionId,
+                    placement
+                })
+                return;
+            }
+
+            console.warn("Ad did not show. Skipping.");
+            if (typeof callbacks.noShow !== "function") {
+
+                return;
+            }
+
+            callbacks.noShow();
+
+        }, 500);
+
+        params.beforeAd = function () {
             adShown = true;
             callbacks.beforeAd();
-        },
-        afterAd: callbacks.afterAd
+        };
+        params.afterAd = callbacks.afterAd;
+
+        adBreak(params);
+
     }
-
-    if (callbacks.adBreakDone) {
-        params.adBreakDone = callbacks.adBreakDone
-    }
-
-    adBreak(params);
-
 }
 
 window.initWortal = function (callback) {
@@ -109,7 +160,7 @@ window.initWortal = function (callback) {
     let loadGoogleScript = document.createElement("script");
     let clientId = getParameterByName("clientid");
     let debug = getParameterByName("debug");
-    let hostChannelId = getParameterByName("channelid");
+    let hostChannelId = getParameterByName("channelid")
     let hostId = getParameterByName("hostid")
     let freqCap = `${getParameterByName("freqcap") || 30}s`
 
@@ -150,4 +201,17 @@ window.initWortal = function (callback) {
 
     });
 
+}
+
+window.initWortalLink = function (callback) {
+    var loadLinkScript = document.createElement("script");
+    document.head.appendChild(loadLinkScript);
+
+    loadLinkScript.src = "https://lg.rgames.jp/libs/link-game-sdk/1.0/bundle.js";
+    loadLinkScript.onload = function(script) {
+        window.wortalLink = LinkGame
+        if (typeof (callback) === "function") {
+            callback();
+        }
+    };
 }
